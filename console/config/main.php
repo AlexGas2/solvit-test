3724101e8cb444e36ef82e9fc551eb4be612654d
Oleksandr Holub <gas777738@gmail.com>
2022-11-02 20:12:53 +0100

    Add needs params and fixture config

--- console/config/main.php
+++ console/config/main.php
@@ -18,9 +18,11 @@ return [
     ],
     'controllerMap' => [
         'fixture' => [
-            'class' => 'yii\console\controllers\FixtureController',
+            'class' => 'yii\faker\FixtureController',
             'namespace' => 'common\fixtures',
-          ],
+            'templatePath' => '@common/fixtures/templates',
+            'fixtureDataPath' => '@common/fixtures/data',
+        ],
     ],
     'components' => [
         'log' => [
